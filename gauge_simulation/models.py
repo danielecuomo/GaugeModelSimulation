67dc8a02f7897480836797770b8b1bc3836426c9
Daniele Cuomo <daniele.cuomo@unina.it>
2025-12-05 15:29:49 +0200

    Update models.py

--- gauge_simulation/models.py
+++ gauge_simulation/models.py
@@ -1,11 +1,9 @@
 from .hamiltonian import Hamiltonian
 from .core import tfim_generalized, xy_generalized
 
-def build_hamiltonian(model, lattice_shape, trotter_steps=2):
-    rows, cols = lattice_shape
-    num_qubits = rows * cols
-    dimension = 1 if (rows == 1 or cols == 1) else 2
-
+def build_hamiltonian(model, size, trotter_steps=2):
+    dimension = size[0]
+    num_qubits = size[1]**size[0]
     if model == "tfim":
         sim = tfim_generalized(dimension, num_qubits, trotter_steps)
     else:
